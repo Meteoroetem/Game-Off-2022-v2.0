e16b4461a3f3f3632b005d60cbb5df1dfc478de7
Meteoroetem <114942884+Meteoroetem@users.noreply.github.com>
2022-12-03 13:43:38 +0200

    Fixing performance Issues and final touches

--- main.py
+++ main.py
@@ -3,32 +3,32 @@ from wall import wall
 
 pygame.init()
 
-fps = 1000
+fps = 60
 
 size = width, height = 1280, 720
 backgroundColor = 138, 255, 206
 screen = pygame.display.set_mode(size)
 
-mainFont = pygame.font.Font("Assets/FastinaOotsineDEMO/FastinaOotsineDEMO.otf", 120)
+mainFont = pygame.font.Font("Assets/Fonts/FastinaOotsineDEMO.otf", 120)
 score = 0
 
-background = pygame.image.load("Assets/newPyGameBackground.png")
+background = pygame.image.load("Assets/Sprites/newPyGameBackground.png")
 screen.blit(background,(0,0))
 
-player = pygame.image.load("Assets/python logo square ratio 150p.png")
+player = pygame.image.load("Assets/Sprites/python logo square ratio 150p.png")
 playerRect = player.get_rect()
 playerRect.center = (screen.get_rect().centerx - 150, screen.get_rect().centery)
 playerAngle = 0
-
-wall1 = wall()
-wall2 = wall(pos=(2560,0))
-wall3 = wall(pos=(3840,0))
-
-walls = [wall1, wall2 ,wall3]            #print(wall1.rects[0].topleft)
-
-velocity = vx, vy = 0, 0
-acceleration = 0.3
+playerColidingRect = pygame.Rect(playerRect.topleft, [playerRect.width-20, playerRect.height-20])
+newIsPassing = False
+oldIsPassing = False
+velocity = vx, vy = 17, 0
+acceleration = 0.8
 canJump = True
+jumpForce = 10
+jumpTime = 150
+
+walls = [wall(), wall((2560,0)) ,wall((3840,0))]            #print(wall1.rects[0].topleft)
 
 isJumping = False
 jumpTimer = pygame.USEREVENT + 1
@@ -40,7 +40,7 @@ while True:
         #getting inputs (mouse, space bar and up key) and starting to jump
         elif event.type == pygame.MOUSEBUTTONDOWN or (event.type == pygame.KEYDOWN and (pygame.key.get_pressed()[pygame.K_SPACE] or pygame.key.get_pressed()[pygame.K_UP])): 
             if not isJumping:
-                pygame.time.set_timer(jumpTimer, 200)
+                pygame.time.set_timer(jumpTimer, jumpTime)
                 isJumping = True
                 
         elif event.type == jumpTimer: 
@@ -52,26 +52,26 @@ while True:
             
     
     if isJumping == True:
-        vy = -5
-        playerAngle -= 8.57
+        vy = -jumpForce
+        #calculate the required amount of rotation to rotate 180 degrees
+        playerAngle -= 180/(fps*(jumpTime/1000))
+    print(playerAngle)
         
-    for theWall in walls:
-        for rect in theWall.rects:
-            colidingRect = pygame.Rect(playerRect.topleft, [playerRect.width-20, playerRect.height-20])
-            colidingRect.center = playerRect.center
-            if colidingRect.colliderect(rect):
-                print("Colliding!")
-                score = 0
+    #for theWall in walls:
+        #for rect in theWall.rects:
+            #colidingRect = pygame.Rect(playerRect.topleft, [playerRect.width-20, playerRect.height-20])
+            #colidingRect.center = playerRect.center
+            #if colidingRect.colliderect(rect):
+                #score = 0
 
-    for theWall in walls:
-        if playerRect.centerx == theWall.position[0]+50:
-            score += 10
+    #for theWall in walls:
+        #if playerRect.centerx == theWall.position[0]+50:
+            #score += 10
 
-
+    #If player exits screen
     if playerRect.top > screen.get_height():
-        #playerRect.center = (screen.get_rect().centerx - 150, screen.get_rect().centery)
         playerRect.top = screen.get_height()
-        vy = -15
+        vy = -20
         if score != 0: score -= 5
     if playerRect.top < 0:
         playerRect.top = 0
@@ -80,18 +80,8 @@ while True:
     #Physics calculations
     vy += acceleration
     velocity = vx, vy
-    playerRect = playerRect.move(velocity)
-
-    for theWall in walls:
-        if theWall.position[0] < -theWall.image.get_rect().width:
-            walls.remove(theWall)
-            walls.append(wall((3840,0)))
-        theWall.position -= pygame.Vector2(10,0)
-        for rect in theWall.rects:
-            rect.left -= 10
-
-        
-            
+    playerRect = playerRect.move((0, vy))
+    playerColidingRect.center = playerRect.center
 
     #Screen update
     screen.fill(backgroundColor)
@@ -99,6 +89,25 @@ while True:
     screen.blit(pygame.transform.rotate(player, playerAngle), playerRect)
     
     for theWall in walls:
+        #When theWall exits screen instanciate a new one in the front
+        if theWall.position[0] < -theWall.image.get_rect().width:
+            walls.remove(theWall)
+            walls.append(wall((3840,0)))
+
+        theWall.Move(-vx)
+        #print(walls[0].parentRect,walls[0].position,playerRect)
+
+        for rect in theWall.rects:
+            if playerColidingRect.colliderect(rect):
+                score = 0
+
+        newIsPassing = playerRect.colliderect(walls[0].parentRect) or playerRect.colliderect(walls[1].parentRect) or playerRect.colliderect(walls[2].parentRect)
+        #print(newIsPassing, oldIsPassing)
+        if newIsPassing and not oldIsPassing:
+            score += 10
+        
+        oldIsPassing = newIsPassing
+            
         if theWall.position[0] < 1280 or theWall.position[0] > 0:
             screen.blit(theWall.image, theWall.position)
 
--- wall.py
+++ wall.py
@@ -8,18 +8,26 @@ class wall:
     spriteNum = 0
     image = pygame.surface.Surface((1,1))
     rects = [pygame.Rect(0,0,1,1),pygame.Rect(0,0,1,1)]
+    parentRect = image.get_rect()
+
+    def Move(self, xvalue):
+        self.position += pygame.Vector2(xvalue,0)
+        for rect in self.rects:
+            rect.left += xvalue
+        self.parentRect.left += xvalue
 
 
     def __init__(self, pos = (1280,0)):
         self.position = pos
         self.spriteNum = random.randint(1,3)
         if self.spriteNum == 1:
-            self.image = pygame.image.load("Assets/semicolon_mistake.png")
+            self.image = pygame.image.load("Assets/Sprites/semicolon_mistake.png")
             self.rects = [pygame.Rect(pos[0] + 82, pos[1] + 2, 135, 144), pygame.Rect(pos[0] + 100, pos[1] + 404, 111, 270)]
         elif self.spriteNum == 2:
-            self.image = pygame.image.load("Assets/bracketsMistake.png")
+            self.image = pygame.image.load("Assets/Sprites/bracketsMistake.png")
             self.rects = [pygame.Rect(pos[0], pos[1] + 10, 61, 250), pygame.Rect(pos[0] + 191, pos[1] + 459, 55, 251)]
         elif self.spriteNum == 3:
-            self.image = pygame.image.load("Assets/newIntMistake.png")
+            self.image = pygame.image.load("Assets/Sprites/newIntMistake.png")
             self.rects = [pygame.Rect(pos[0] + 241, pos[1] + 20, 101, 109), pygame.Rect(pos[0] + 19, pos[1] + 294, 115, 111), pygame.Rect(pos[0] + 230, pos[1] + 593, 124, 111)]
-        
+        self.parentRect = self.image.get_rect()
+        self.parentRect.topleft = pos      
